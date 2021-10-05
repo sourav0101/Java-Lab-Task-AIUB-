0af1bef9b33163710d5b4493545db5a5d944fb67
Sourav Das <33197805+sourav0101@users.noreply.github.com>
2021-10-06 00:11:24 +0600

    Update BookShop.java

--- Lab task 8/BookShop.java
+++ Lab task 8/BookShop.java
@@ -3,61 +3,61 @@ public class BookShop implements BookShopOperations
 {
 	private String name;
 	private Book books[] = new Book[100];
-	public void setBookShopName(String name){
-		this.name = name;
+	public void setBookShopName(String name)
+	{
+	     this.name = name;
 	}
 	public String getBookShopName()
 	{
-		return this.name;
+	     return this.name;
 	}
 	
 	BookShop(){ }
-	
-	BookShop(String name){
-		this.name = name;
+	BookShop(String name)
+	{
+	    this.name = name;
+	}
+	public boolean insertBook(Book boo)
+	{
+	    boolean flag = false;
+	    for(int i=0; i<books.length; i++)
+	    {
+		if(books[i] == null)
+	        {
+		    books[i] = boo;
+		    flag = true;
+		    break;
+		}
+	    }
+	    return flag;
 	}
 	
-	
-	
-	public boolean insertBook(Book boo){
-		boolean flag = false;
-		for(int i=0; i<books.length; i++)
-		{
-			if(books[i] == null)
-			{
-				books[i] = boo;
-				flag = true;
-				break;
-			}
-		}
-		return flag;
-	}
-	
-	public boolean removeBook(Book boo){
-		boolean flag = false;
-		for(int i=0; i<books.length; i++)
-		{
-			if(books[i] == boo){ books[i] = null;
-				flag = true;
-				break;
-			}
-		}
-		return flag;
+	public boolean removeBook(Book boo)
+	{
+	    boolean flag = false;
+	    for(int i=0; i<books.length; i++)
+	    {
+		if(books[i] == boo){ books[i] = null;
+		flag = true;
+		break;
+	     }
+	  }
+	    return flag;
 	}
 		
 	public Book searchBook(String isbn){
-		Book boo = null;
-		for(int i =0; i<books.length; i++)
+	Book boo = null;
+	for(int i =0; i<books.length; i++)
 		{
-			if(books[i] != null)
+		if(books[i] != null)
+		{
+			if(books[i].getIsbn() == isbn)
 			{
-				if(books[i].getIsbn() == isbn)
-				{
-					boo = books[i];
-					break;
-				}
+				boo = books[i];
+				break;
 			}
-		}
+		 }
+	   }
 		return boo;
 	}
 	
