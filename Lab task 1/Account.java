ad3b689a656364e1f40b00d07dde078eae044491
Sourav Das <33197805+sourav0101@users.noreply.github.com>
2021-10-05 23:01:08 +0600

    Update Account.java

--- Lab task 1/Account.java
+++ Lab task 1/Account.java
@@ -1,1 +1,39 @@
+public class Account {
+ 
+    private  int accountNumber;
+    private  String accountHolderName;
+    private  double balance;
 
+    public void setAccountNumber(int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+  
+    public void setAccountHolderName(String accountHolderName) {
+        this.accountHolderName = accountHolderName;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+ 
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public String getAccountHolderName() {
+        return accountHolderName;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void showDetails(){
+        
+        System.out.println("Account Name:"+ getAccountNumber());
+        System.out.println("Account HolderName:"+ getAccountHolderName());
+        System.out.println("Balance:"+getBalance());
+    }
+    
+        
+}
