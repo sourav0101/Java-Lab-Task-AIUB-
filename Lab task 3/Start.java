366e04d48fe1fdbe20a11ec18cf7d5b3a5b7adb4
Sourav Das <33197805+sourav0101@users.noreply.github.com>
2021-10-05 23:44:45 +0600

    Update Start.java

--- Lab task 3/Start.java
+++ Lab task 3/Start.java
@@ -1,6 +1,6 @@
 import java.lang.*; 
 public class Start{
-	public static void main(String args[])
+public static void main(String args[])
 	{
 		StoryBook s = new StoryBook(); 
 		StoryBook s2 = new StoryBook();
